5e9f1ed67ebd1ec48f2ad25813d3333b8ae611cc
Kaja Anna <kaja.szokalska@gmail.com>
2021-12-13 10:59:20 +0100

    replace Line element with :before

--- src/navigation/Menu.js
+++ src/navigation/Menu.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashLink } from 'react-router-hash-link';
-
-import Line from '../shered/Line'
-
 
 import './Menu.css'
 
 function Menu(props) {
+  const [ showLine, setShowLine ] = useState(false);
+    useEffect(() => {
+        let options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.7
+          }
+          function onScreen (entries) {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                setShowLine(true);
+              } else {
+                setShowLine(false);
+              }
+            });
+          }
+          let lineObserver = new IntersectionObserver( onScreen, options);
+          lineObserver.observe(document.getElementById("menu"));
+    },[showLine]);
   return (
-    <div className="menu">
+    <div id="menu" className="menu">
       <ul>
-        <li>
+        <li className={showLine && "menu__line"}>
           <HashLink className="menu_link" smooth to="/#home">
             home
           </HashLink>
         </li>
-        <Line />
-        <li>
+       
+        <li className={showLine && "menu__line"}>
           <HashLink className="menu_link" smooth to="/#about">
             about
           </HashLink>
         </li>
-        <Line />
-        <li>
+       
+        <li className={showLine && "menu__line"}>
           <HashLink className="menu_link" smooth to="/#projects">
             projects
           </HashLink>
         </li>
-        <Line />
-        <li>
+       
+        <li className={showLine && "menu__line"}>
           <HashLink className="menu_link" smooth to="/#skills">
             skills
           </HashLink>
         </li>
-        <Line />
-        <li>
+      
+        <li className={showLine && "menu__line"}>
           <HashLink className="menu_link" smooth to="/#contact">
             contact
           </HashLink>
